190ca67f63fcd04060df94c00eef09769cc60e7a
Guillaume Lagarde <tinne.lesage@gmail.com>
2020-04-23 18:48:25 +0200

    test++

--- code/union_find.pyx
+++ code/union_find.pyx
@@ -1,5 +1,3 @@
-# Author: Gael Varoquaux <gael.varoquaux@normalesup.org>
-
 import numpy as np
 cimport numpy as np
 cimport cython
@@ -35,8 +33,6 @@ class UN():
 
 cdef class UnionFind():
 
-    # cdef ITYPE_t next_label
-    
     cdef ITYPE_t[:] parent
     cdef ITYPE_t[:] size
     cdef ITYPE_t[:] succ
@@ -48,8 +44,8 @@ cdef class UnionFind():
 
       
         
-    # @cython.boundscheck(False)
-    # @cython.nonecheck(False)
+    @cython.boundscheck(False)
+    @cython.nonecheck(False)
     cdef void union(self, ITYPE_t m, ITYPE_t n):
         cdef ITYPE_t r_m
         cdef ITYPE_t r_n
@@ -72,10 +68,10 @@ cdef class UnionFind():
     cdef ITYPE_t find(self, ITYPE_t n):
         cdef ITYPE_t p
         p = n
-        # find the highest node in the linkage graph so far
+        # find the root
         while self.parent[n] != -1:
             n = self.parent[n]
-        # provide a shortcut up to the highest node
+        # path compression
         while self.parent[p] != n:
             p, self.parent[p] = self.parent[p], n
         return n
