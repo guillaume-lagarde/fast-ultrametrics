f9c97c2c6350e286cac69662324c4feda910f311
rverclos <remi.de.joannis.de.verclos@ens-lyon.org>
2020-04-27 15:09:40 +0200

    More tests

--- code/test.py
+++ code/test.py
@@ -7,7 +7,7 @@ class TestSum(unittest.TestCase):
     def test_test(self):
         self.assertEqual(2 + 2, 4)
 
-    def test_single_linkage_label(self):
+    def test_single_linkage_label_squares(self):
         K = 6
         P = np.array([[i**2] for i in range(K)])
         mst = np.array([[i,i+1] for i in range(K-1)])
@@ -22,6 +22,24 @@ class TestSum(unittest.TestCase):
                     [ 7., 3.,  45., 4.],
                     [ 8., 4.,  80., 5.],
                     [ 9., 5., 125., 6.]]
+        self.assertEqual( [list(node) for node in res], expected)
+
+    def test_single_linkage_label2(self):
+        P = np.array([[-150.], [-110.], [-50.], [0.], [1.], [70.]])
+        N = len(P)
+        mst = np.array([[i,i+1] for i in range(N-1)])
+
+        cut_weights = cut_weight(P, mst)
+        self.assertEqual(
+            list(cut_weights),
+            [5.*40., 5.*150., 5.*50., 5.*1., 5.*80.],
+        )
+        res = single_linkage_label(N, mst, cut_weights)
+        expected = [[ 3., 4.,   5., 2.],
+                    [ 0., 1., 200., 2.],
+                    [ 2., 6., 250., 3.],
+                    [ 8., 5., 400., 4.],
+                    [ 7., 9., 750., 6.]]
         self.assertEqual( [list(node) for node in res], expected)
 
 #    def other(self):
