b2b7985ba3dbba7998afb11334e866594c699a26
guillaume-lagarde <tinne.lesage@gmail.com>
2020-04-27 15:51:32 +0200

    example real dataset

--- code/graphic.py
+++ code/graphic.py
@@ -2,16 +2,21 @@ from union_find import *
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram
 import random
-K = 10
-# P = np.array([[random.random()*1000] for i in range(K)])
-P = np.array([[i**2] for i in range(K)])
+
+file_name = "datasets/IRIS.csv"
+
+X = np.genfromtxt(file_name, delimiter=",")
+K = 1000
+P = np.array([[random.random()*1000] for i in range(K)])
+# P = np.array([[i**2] for i in range(K)])
 mst = np.array([[i,i+1] for i in range(K-1)])
 N = len(P)
 assert(len(mst) == N-1)
 
-cut_weights = cut_weight(P, mst)
-res = single_linkage_label(N, mst, cut_weights)
-res = all_together(P)
+# cut_weights = cut_weight(P, mst)
+# res = single_linkage_label(N, mst, cut_weights)
+# res = all_together(P)
+res =all_together(X)
 print("############",res)
 plt.title('Hierarchical Clustering Dendrogram')
 
