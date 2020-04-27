40c55bd228fba32cf83151bee5c9dc2f58fe5ebc
rverclos <remi.de.joannis.de.verclos@ens-lyon.org>
2020-04-27 12:54:09 +0200

    improve test

--- code/test.py
+++ code/test.py
@@ -1,30 +1,34 @@
 import unittest
 import numpy as np
 from union_find import *
-K = 6
+
 class TestSum(unittest.TestCase):
 
     def test_test(self):
         self.assertEqual(2 + 2, 4)
 
     def test_single_linkage_label(self):
+        K = 6
         P = np.array([[i**2] for i in range(K)])
-        print("points", P)
         mst = np.array([[i,i+1] for i in range(K-1)])
+        N = len(P)
+        assert(len(mst) == N-1)
 
-        print("mst", mst)
+        cut_weights = cut_weight(P, mst)
+        self.assertEqual(list(cut_weights), [5. * (i+1)**2 for i in range(K-1)])
+        res = single_linkage_label(N, mst, cut_weights)
+        expected = [[ 0., 1.,   5., 2.],
+                    [ 6., 2.,  20., 3.],
+                    [ 7., 3.,  45., 4.],
+                    [ 8., 4.,  80., 5.],
+                    [ 9., 5., 125., 6.]]
+        self.assertEqual( [list(node) for node in res], expected)
+
+#    def other(self):
         # P = np.array([[0., 0.], [0., 2.], [0., 3.], [3., 3.]])
         # mst = np.array([[1, 2], [0, 1], [3, 2]])
         
-        N = len(P)
-        assert(len(mst) == N-1)
-        print(N)
         
-        cut_weights = cut_weight(P, mst)
-        print("cut weights: {}".format(cut_weights))
-        res = single_linkage_label(N, mst, cut_weights)
-        print("res: {}".format(res))
-
 if __name__ == '__main__':
     unittest.main()
     
