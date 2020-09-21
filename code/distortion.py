bcff23f4d9355eb506d7032bd0df6d130fa4f5c3
guillaume-lagarde <guillaume.lagarde@gmail.com>
2020-09-21 16:56:58 +0200

    efficient RMQ with sparse tables

--- code/distortion.py
+++ code/distortion.py
@@ -1,3 +1,5 @@
+import math
+
 # input: array left cluster, right cluster, delta, size subtree
 def infix_order(tree):
     stack = [(-1,-1, -1)]
@@ -20,28 +22,78 @@ def infix_order(tree):
             
     return res
 
+# OLD one
+# class RMQ:
+#     def __init__(self, tree):
+#         order = infix_order(tree)
+#         self.tree = tree
+#         self.n = len(tree)+1
+#         self.depth = [x for (_,x) in order]
+#         self.indices = [x for (x,_) in order]
+#         self.positions = [0]*len(order)
+#         for t, i in enumerate(self.indices):
+#             self.positions[i] = t
+
+#     def search(self, u, v):
+#         iu, iv = self.positions[u], self.positions[v]
+#         if iu > iv:
+#             iu, iv = iv, iu
+#         im = iu
+#         m = self.depth[im]
+#         for i in range(iu+1, iv+1):
+#             mc = self.depth[i]
+#             if mc < m:
+#                 im, m = i, mc
+#         return self.indices[im]
+
+#     def dist(self, u, v):
+#         if u!= v:
+#             return self.tree[self.search(u, v)- self.n][2]
+#         return 0
+
+
 class RMQ:
     def __init__(self, tree):
         order = infix_order(tree)
         self.tree = tree
         self.n = len(tree)+1
         self.depth = [x for (_,x) in order]
+        print(self.depth)
         self.indices = [x for (x,_) in order]
         self.positions = [0]*len(order)
         for t, i in enumerate(self.indices):
             self.positions[i] = t
 
+        # sparse table for efficient search
+        self.N = len(order)
+        self.K = math.floor(math.log2(self.N)) # max exponent
+        self.st = [[i]*(self.K+1) for i in range(self.N)] # st[i][j] = index of min between [i, i + 2**j-1]
+
+        # filling the sparse table
+        for j in range(1,self.K+1):
+            for i in range(0, self.N):
+                if i + (1 <<(j-1) ) >= self.N: break
+                i1, i2 = self.st[i][j-1], self.st[i+(1<<(j-1))][j-1]
+                print(i1, i2)
+                if self.depth[i1] < self.depth[i2]:
+                    self.st[i][j] = i1
+                else:
+                    self.st[i][j] = i2
+        print("##########")
+        print(self.st)
+        print("###########")
+                    
     def search(self, u, v):
         iu, iv = self.positions[u], self.positions[v]
         if iu > iv:
             iu, iv = iv, iu
-        im = iu
-        m = self.depth[im]
-        for i in range(iu+1, iv+1):
-            mc = self.depth[i]
-            if mc < m:
-                im, m = i, mc
-        return self.indices[im]
+
+        j = math.floor(math.log2(iv-iu+1))
+        i1, i2 = self.st[iu][j], self.st[iv-(1<<j)+1][j]
+        if self.depth[i1] < self.depth[i2]:
+            return self.indices[i1]
+        else:
+            return self.indices[i2]
 
     def dist(self, u, v):
         if u!= v:
